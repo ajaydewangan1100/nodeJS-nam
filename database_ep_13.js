f777506b994d1c9e4bb5b6fa61bfe315a0daf92c
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-09-28 23:59:30 +0530

    Database started, mongodb connected, inserted, read data

---
+++ database_ep_13.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require("mongodb");
+
+const DB_URI =
+  "mongodb+srv://ajaydewangan1100:<Password_here>@cluster0.rrg8o.mongodb.net/";
+
+const client = new MongoClient(DB_URI);
+
+const dbName = "Namastedev_start";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("start");
+
+  //   INSERT DATA
+  const data = {
+    name: "Piku",
+    age: 25,
+    city: "New York",
+    country: "USA",
+  };
+
+  //   const insertResult = await collection.insertMany([data]);
+
+  // READ
+  //   const findResult = await collection.find({}).toArray();
+  //   console.log("Found documents =>", findResult);
+
+  //   COUNT DOCUMENTS
+  //   const resultCount = await collection.countDocuments({});
+  //   console.log("Total documents =>", resultCount);
+
+  //     FIND ALL DOCUMNETS WITH A FILTER OF NAME
+  const result = await collection.find({ name: "Piku" }).toArray();
+  console.log(result);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
